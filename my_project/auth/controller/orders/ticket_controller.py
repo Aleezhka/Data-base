4ec4e384c58e7568dc53ef8cb248dbccab31f365
Aleezhka <151876861+Aleezhka@users.noreply.github.com>
2024-11-28 05:48:11 +0200

    Лабораторна 4

---
+++ my_project/auth/controller/orders/ticket_controller.py
@@ -0,0 +1,20 @@
+from typing import List
+from my_project.auth.service import TicketService
+from my_project.auth.controller.general_controller import GeneralController
+
+class TicketController(GeneralController):
+    _service = TicketService()
+
+    def get_tickets_by_user_id(self, user_id: int) -> List[dict]:
+        tickets = self._service.get_tickets_by_user_id(user_id)
+        return [ticket.put_into_dto() for ticket in tickets]
+
+    def get_tickets_by_flight_id(self, flight_id: int) -> List[object]:
+        return list(map(lambda x: dict(x), self._service.get_tickets_by_flight_id(flight_id)))
+
+    def get_tickets_by_user_id(self, user_id: int):
+        """
+        Отримує всі квитки користувача.
+        """
+        tickets = self._service.get_tickets_by_user(user_id)
+        return [ticket.put_into_dto() for ticket in tickets]
